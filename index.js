209bb27cde3ebb3a574738da47654b66e1149cf1
rsudekum <bobby@mapbox.com>
2015-04-15 16:50:37 -0700

    add offline handling

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-require('./lib/ui')();
-require('./lib/map')();
+var React = require('react');
+var UI = require('./lib/ui');
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <UI/>
+      </div>
+    );
+  }
+});
+
+React.render(<App />, document.getElementById('app'));
